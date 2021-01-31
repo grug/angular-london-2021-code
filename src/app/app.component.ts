5853b660adb22dc0f6d60a27e44c82144335d9b9
Dave Cooper <dave@davecooper.dev>
2021-01-31 19:49:05 +0000

    Working with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+type Widget = {
+  id: number;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  widgets: Widget[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('http://localhost:3000/widgets')
+      .subscribe((widgets: Widget[]) => (this.widgets = widgets));
+  }
+
+  addNewWidget(): void {
+    this.http
+      .get('http://localhost:3000/new-widget')
+      .subscribe((widget: Widget) => {
+        this.widgets = [...this.widgets, widget];
+      });
+  }
 }
