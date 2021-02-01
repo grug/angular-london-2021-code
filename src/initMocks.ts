2e7b52b98256ac0ef5a9431aee3d3830665261a4
Dave Cooper <dave@davecooper.dev>
2021-02-01 20:10:55 +0000

    Ready for demo

---
+++ src/initMocks.ts
@@ -0,0 +1,3 @@
+const initMocks = () => {};
+
+export { initMocks };
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { initMocks } from './initMocks';
 
 if (environment.production) {
   enableProdMode();
+} else {
+  initMocks();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
