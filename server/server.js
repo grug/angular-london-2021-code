f0aa75bb19ef124689472cfcb60d18887b21d084
Dave Cooper <dave@davecooper.dev>
2021-02-01 20:39:38 +0000

    Ready for demo

--- server/server.js
+++ server/server.js
@@ -4,13 +4,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-let latestWidgetId = 10;
-
 app.get("/widgets", (req, res) =>
   res.json([...Array(10)].map((_, i) => ({ id: i + 1 })))
 );
-
-app.get("/new-widget", (req, res) => res.json({ id: ++latestWidgetId }));
 
 app.listen(3000, () =>
   console.log(`Dave's widget factory: ready for business`)
